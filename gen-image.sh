8eb0a3ec8103330dc7f8076e576e0b224bbd42a1
Eleanor Young <developer@nausicaea.net>
2025-03-10 09:37:10 +0100

    Moved the gen-image.sh script to the root

--- gen-image.sh
+++ gen-image.sh
@@ -4,8 +4,9 @@ set -e
 
 IMAGE_TAG="nausicaea/debian-auto:latest"
 SCRIPT_DIR=$(dirname "$0")
+CONTEXT_DIR="${SCRIPT_DIR}/os-install"
 
-docker build -t "$IMAGE_TAG" "$SCRIPT_DIR"
+docker build -t "$IMAGE_TAG" "$CONTEXT_DIR"
 exec docker run --rm \
     -v "$SCRIPT_DIR/cache:/cache" -v "$SCRIPT_DIR/artifacts:/artifacts" \
     "$IMAGE_TAG" "$@"
