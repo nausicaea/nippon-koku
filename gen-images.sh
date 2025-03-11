90906b50cebf58b0a055984454f11de1f71693da
Eleanor Young <developer@nausicaea.net>
2025-03-11 16:46:58 +0100

    Added input from a file

--- gen-images.sh
+++ gen-images.sh
@@ -1,8 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-# Required arguments (provided via getotps)
+function build_image() {
+    OLD_IFS="$IFS"
+    IFS=","
+    read -r 'host_name' 'host_arch' 'host_boot_device' 'kube_apiserver_url' <<< "$9"
+    IFS="$OLD_IFS"
+
+    if [ "x$host_name" = "x" -o "x$host_arch" = "x" -o "x$host_boot_device" = "x" -o "x$kube_apiserver_url" = "x" ]; then
+        echo "Wrong host data format: Expected CSV data with four columns, got: '$9'"
+        return 1
+    fi
+
+    docker run --rm -q \
+        -v "$1/cache:/cache" -v "$1/artifacts:/artifacts" \
+        "$2" \
+        -b "$host_boot_device" -H "$host_name" -a "$host_arch" -B "$3" \
+        -r $(openssl passwd -6 $(op read --account="$4" "$5")) \
+        -v $(op read --account="$4" "$6") \
+        -e "$7" -s "$8" -k "$kube_apiserver_url"
+}
+
+# Positional arguments (provided via getotps)
 # HOST_DATA=""
 
 # Required environment variables (we assume that these environment variables are present)
@@ -12,13 +32,14 @@ set -e
 
 # Arguments with defaults
 BOOTSTRAP_BRANCH="${BOOTSTRAP_BRANCH:-main}"
+INPUT_FILE=""
 
 # Hardcoded variables
 SCRIPT_DIR=$(dirname "$0")
 CONTEXT_DIR="${SCRIPT_DIR}/os-install"
 IMAGE_TAG="nausicaea/debian-auto:latest"
 
-while getopts ':h' 'opt'; do
+while getopts ':hi:' 'opt'; do
     case $opt in
         h)
             printf \
@@ -34,13 +55,22 @@ USAGE:
 
 OPTIONS:
   -h                            Print this usage message and exit.
+  -i INPUT_FILE                 Optionally specify a file with host
+                                data triplets (each separated by 
+                                newlines). The special value "-" 
+                                indicates that you would like 
+                                to read data from stdin.
 
 ARGUMENTS:
-  HOST_DATA                     For every image to generate, specify 
-                                a hostname, architecture, and boot 
-                                device triplet, e.g. "debian:arm64:/dev/sda"
+  HOST_DATA                     For every image to generate, 
+                                specify hostname, architecture, 
+                                boot device, and Kube API server 
+                                URL as CSV format.
 '
             exit 0
+            ;;
+        i)
+            INPUT_FILE="${OPTARG}"
             ;;
         :)
             echo "Option -${OPTARG} requires an argument."
@@ -52,16 +82,33 @@ ARGUMENTS:
             ;;
     esac
 done
+shift $((OPTIND-1))
 
-docker build -q -t "$IMAGE_TAG" "$CONTEXT_DIR"
+export DOCKER_CLI_HINTS=false
+docker build -q -t "$IMAGE_TAG" "$CONTEXT_DIR" >/dev/null
+
+if [ -f "$INPUT_FILE" ]; then
+    while read 'host_data'; do
+        if ! [ -z "$host_data" ]; then
+            build_image "$SCRIPT_DIR" "$IMAGE_TAG" "$BOOTSTRAP_BRANCH" "$OP_ACCOUNT_SERVER" \
+                "$OP_ROOT_PW_ID" "$OP_VAULT_ID" "$GIT_AUTHOR_EMAIL" "$GIT_AUTHOR_SSH_PUB" \
+                "$host_data"
+        fi
+    done < "$INPUT_FILE"
+elif [ "$INPUT_FILE" = "-" ]; then
+    while IFS= read -r 'host_data'; do
+        if ! [ -z "$host_data" ]; then
+            build_image "$SCRIPT_DIR" "$IMAGE_TAG" "$BOOTSTRAP_BRANCH" "$OP_ACCOUNT_SERVER" \
+                "$OP_ROOT_PW_ID" "$OP_VAULT_ID" "$GIT_AUTHOR_EMAIL" "$GIT_AUTHOR_SSH_PUB" \
+                "$host_data"
+        fi
+    done
+fi
 
 for host_data in "$@"; do
-    IFS=":" read -r host_name host_arch host_boot_device <<< "$host_data"
-    docker run --rm -q \
-        -v "$SCRIPT_DIR/cache:/cache" -v "$SCRIPT_DIR/artifacts:/artifacts" \
-        "$IMAGE_TAG" \
-        -b "$host_boot_device" -H "$host_name" -a "$host_arch" -B "$BOOTSTRAP_BRANCH" \
-        -r $(openssl passwd -6 $(op read --account="$OP_ACCOUNT_SERVER" "$OP_ROOT_PW_ID")) \
-        -v $(op read --account="$OP_ACCOUNT_SERVER" "$OP_VAULT_ID") \
-        -e "$GIT_AUTHOR_EMAIL" -s "$GIT_AUTHOR_SSH_PUB" -k "$KUBE_APISERVER_URL"
+    if ! [ -z "$host_data" ]; then
+        build_image "$SCRIPT_DIR" "$IMAGE_TAG" "$BOOTSTRAP_BRANCH" "$OP_ACCOUNT_SERVER" \
+            "$OP_ROOT_PW_ID" "$OP_VAULT_ID" "$GIT_AUTHOR_EMAIL" "$GIT_AUTHOR_SSH_PUB" \
+            "$host_data"
+    fi
 done
