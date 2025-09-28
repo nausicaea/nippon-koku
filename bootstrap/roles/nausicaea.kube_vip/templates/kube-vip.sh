53b91dba7bf5e54336455c623b9c6f4e3daa98e4
Eleanor Young <developer@nausicaea.net>
2025-09-28 16:39:46 +0200

    refactor: separate nausicaea.common into nausicaea.ansible, nausicaea.hardening, nausicaea.manual_admin, and nausicaea.etckeeper to clearly separate individual concerns

---
+++ bootstrap/roles/nausicaea.kube_vip/templates/kube-vip.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+KVVERSION='v1.0.0'
+KVIMAGE="ghcr.io/kube-vip/kube-vip:$KVVERSION"
+
+PODMAN_VERSION=$(podman --version 2> /dev/null || true )
+CTR_VERSION=$(ctr --version 2> /dev/null || true )
+
+printf 'podman: "%s", containerd: "%s"\n' "$PODMAN_VERSION" "$CTR_VERSION"
+
+if [ -n "$PODMAN_VERSION" ]; then
+    exec podman run --rm --network=host "$KVIMAGE" "$@"
+elif [ -n "$CTR_VERSION" ]; then
+    ctr image pull "$KVIMAGE"
+    exec ctr run --rm --net-host "$KVIMAGE" vip /kube-vip "$@"
+fi
+
