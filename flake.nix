bd8efd60e74bc1fe71a32ce5f4db544ce394baea
Eleanor Young <developer@nausicaea.net>
2026-02-23 10:30:36 +0100

    chore: update the ansible version to 2.19.4 / 12.x

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Ansible development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/363465f8f9d3b335f9ddb86a895023974d678a9d";
+    nixpkgs.url = "github:nixos/nixpkgs/dc205f7b4fdb04c8b7877b43edb7b73be7730081";
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
