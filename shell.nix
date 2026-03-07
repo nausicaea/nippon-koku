8dffe79850746f830cb83505053721a25d2a53ff
Eleanor Young <developer@nausicaea.net>
2026-03-07 21:04:45 +0100

    chore: I don't need types-passlib anymore

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 let
     nixpkgs = fetchTarball { name = "nixpkgs-25.11"; url = "https://github.com/NixOS/nixpkgs/archive/nixos-25.11.tar.gz"; sha256 = "0rb04nkqgcglpjnm418qr59y0i6mjlvgn7j1hxhgl48dpavbs36b"; };
     pkgs = import nixpkgs { config = {}; overrides = []; };
-    types-passlib = pkgs.python313Packages.callPackage ./nix/types-passlib.nix {};
 in
 pkgs.mkShell {
     name = "Ansible and Rust Development Environment";
